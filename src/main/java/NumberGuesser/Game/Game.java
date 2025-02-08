c1c31fcb6c536cfebea3a142d949076644dfa829
Dmytro Dzhusov <dmytro.dzhusov@gmail.com>
2025-02-08 20:28:32 +0100

    Game manager is almost completed and game logic is done

--- src/main/java/NumberGuesser/Game/Game.java
+++ src/main/java/NumberGuesser/Game/Game.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Game {
     private Difficulty difficulty;
     private int numberToGuess;
-    private final Scanner scanner;
+    private Scanner scanner;
     private final Random random;
     private final int minRandomBound = 1;
     private final int maxRandomBound = 100;
@@ -21,7 +21,7 @@ public class Game {
     }
 
     public void Start() {
-        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("\nWelcome to the Number Guessing Game!");
         System.out.println("I'm thinking of a number between 1 and 100.");
         System.out.println("You have "
                 + this.difficulty.getNumberOfChances()
@@ -35,38 +35,45 @@ public class Game {
     }
 
     public void SelectDifficulty() {
-        System.out.println("Please select the difficulty level:");
+        System.out.println("\nPlease select the difficulty level:");
         System.out.println("1. Easy (10 chances)");
         System.out.println("2. Normal (5 chances)");
         System.out.println("3. Hard (3 chances)");
 
-        System.out.print("Enter your choice:");
-        try {
-            int chosenDifficulty = scanner.nextInt();
-            this.difficulty = switch(chosenDifficulty) {
-                case 1 -> Difficulty.EASY;
-                case 2 -> Difficulty.MEDIUM;
-                case 3 -> Difficulty.HARD;
-                default -> throw new InvalidDifficulty("Invalid difficulty");
-            };
-            System.out.println("Great! You have selected the " + this.difficulty.getName() + " difficulty level.");
-            System.out.println("Let's start the game!");
-        } catch (Exception e) {
-            System.out.println("You entered an invalid number. Reason: " + e.getMessage());
+        while (true) {
+            System.out.print("Enter your choice:");
+            try {
+                int chosenDifficulty = scanner.nextInt();
+                this.difficulty = switch(chosenDifficulty) {
+                    case 1 -> Difficulty.EASY;
+                    case 2 -> Difficulty.MEDIUM;
+                    case 3 -> Difficulty.HARD;
+                    default -> throw new InvalidDifficulty("Invalid difficulty");
+                };
+                System.out.println("\nGreat! You have selected the " + this.difficulty.getName() + " difficulty level.");
+                System.out.println("Let's start the game!");
+                return;
+            } catch (Exception e) {
+                System.out.println("You entered an invalid number. Reason: " + e.getMessage());
+            }
         }
     }
 
     public boolean IsGuess() {
-        System.out.print("Enter your guess: ");
+        System.out.print("\nEnter your guess: ");
         int guess = 0;
-        try {
-            guess = scanner.nextInt();
-            if (guess < minRandomBound || guess > maxRandomBound) {
-                throw new InvalidGuess("Invalid guess number. Number should be between "
-                        + minRandomBound + " and " + maxRandomBound);
+        while (true) {
+            try {
+                guess = scanner.nextInt();
+                if (guess < minRandomBound || guess > maxRandomBound) {
+                    throw new InvalidGuess("Invalid guess number. Number should be between "
+                            + minRandomBound + " and " + maxRandomBound);
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("You entered an invalid number. Reason: " + e.getMessage());
+                scanner = new Scanner(System.in);
             }
-        } catch (Exception e) {
-            System.out.println("You entered an invalid number. Reason: " + e.getMessage());
         }
         if (guess < numberToGuess) {
             System.out.println("Incorrect! The number is greater than " + guess + ".");
--- src/main/java/NumberGuesser/GameManager/GameManager.java
+++ src/main/java/NumberGuesser/GameManager/GameManager.java
@@ -1,27 +1,69 @@
 package NumberGuesser.GameManager;
 
 import NumberGuesser.Game.Game;
+import NumberGuesser.GameManager.CustomManagerExceptions.InvalidInput;
 
 import java.util.Scanner;
 
 public class GameManager {
-    private Game game;
+    private final Game game;
     private Scanner scanner;
-    private long timeStart;
-    private long timeEnd;
 
-    GameManager() {
+    public GameManager() {
         game = new Game();
         scanner = new Scanner(System.in);
-        timeStart = 0;
-        timeEnd = 0;
     }
 
     public void Rum() {
+        while (true) {
+            MainMenu();
 
+            System.out.print("Enter your choice: ");
+            try {
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1: {
+                        long timeStart = System.currentTimeMillis();
+                        game.Start();
+                        game.SelectDifficulty();
+                        GameProcess();
+                        long timeEnd = System.currentTimeMillis();
+                        System.out.println("Time taken: " + ((timeEnd - timeStart) / 1000) + "s");
+                        break;
+                    }
+                    case 2: ;
+                    case 3: {
+                        System.out.println("Thank you for playing!");
+                        return;
+                    }
+                    default: throw new InvalidInput("Invalid choice");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input: " + e.getMessage());
+                scanner = new Scanner(System.in);
+            }
+        }
     }
 
-    private void PrintMenu() {
-        System.out.println("Welcome to the Number Guessing Game");
+    private void MainMenu() {
+        System.out.println("1. Play");
+        System.out.println("2. High score board");
+        System.out.println("3. Exit game");
+    }
+
+    private void GameProcess() {
+        int attempts = 0;
+        while (!game.IsGuess()) {
+            attempts++;
+            if (attempts >= game.getDifficulty().getNumberOfChances()) {
+                break;
+            }
+        }
+        if (attempts > game.getDifficulty().getNumberOfChances()) {
+            System.out.println("Unfortunately, you lost!");
+        } else {
+            System.out.println("Congratulations! You guessed the correct number in " + (attempts + 1) + " attempts.");
+
+        }
     }
 }
